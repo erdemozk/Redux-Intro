19e6bafbb23bfd6a0d702fc44ef58c88e75394ea
Erdem Özkök <51801996+erdemozk@users.noreply.github.com>
2020-10-06 23:25:41 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { Provider } from 'react-redux';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Selam
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
